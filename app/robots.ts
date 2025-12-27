c88d533ca3940f2923d63bcfafe4d6686a764b1a
Oğuzhan Çamlıca <oguzhancamlica@hotmail.com>
2025-12-27 17:48:14 +0300

    config: robots, sitemap, nextjs version yükseltildi

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://ankatasarim.com'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
+
